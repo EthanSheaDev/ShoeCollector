b528a0c209280a721ca40e169163ae1f08a61c47
Ethan Shea <ethan.shea@estrendo.com>
2017-05-09 14:08:17 +1000

    Initial Commit

---
+++ ShoeCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShoeCollector
+//
+//  Created by Ethan Shea on 9/05/2017.
+//  Copyright © 2017 Ethan Shea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
